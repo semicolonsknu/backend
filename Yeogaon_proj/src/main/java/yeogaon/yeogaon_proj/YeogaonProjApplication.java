2ee7d18cdcdd3aa6a62aa872de374ce3c3c81ca4
KimSongMok <mok0618@naver.com>
2024-03-05 17:35:13 +0900

    first_commit

---
+++ Yeogaon_proj/src/main/java/yeogaon/yeogaon_proj/YeogaonProjApplication.java
@@ -0,0 +1,13 @@
+package yeogaon.yeogaon_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogaonProjApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YeogaonProjApplication.class, args);
+  }
+
+}
---
+++ Yeogaon_proj/src/test/java/yeogaon/yeogaon_proj/YeogaonProjApplicationTests.java
@@ -0,0 +1,13 @@
+package yeogaon.yeogaon_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeogaonProjApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
